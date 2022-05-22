b28717b834808571ffd83dca2827d0d0a20f3aa8
Joe Walker <joewalk102@gmail.com>
2022-05-22 10:14:59 -0700

    Save negative letters between loops

--- main.py
+++ main.py
@@ -22,16 +22,18 @@ def main():
     with open("words.txt", "r") as file:
         words = file.read()
     go_again = True
+    negative_letters = ""
     while go_again:
         raw_text = input("whatcha got? -> ")
         pattern, unknown = parse_input(raw_text)
-        raw_text = input("what's not there? -> ")
+        print("Current omitted letters: " + negative_letters)
+        negative_letters += input("what's not there? -> ")
         pattern = f"^{pattern}$"
         matches = re.findall(pattern, words, re.M)
         final_matches = list()
         if unknown:
             for m in matches:
-                if all([letter in m for letter in unknown]) and not any([letter in m for letter in raw_text]):
+                if all([letter in m for letter in unknown]) and not any([letter in m for letter in negative_letters]):
                     final_matches.append(m)
         pprint(final_matches)
         go_again = input("go again? (y/n) -> ").lower().strip() == "y"
