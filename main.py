5143a6dfa2bbac3f51ccce4b3e03091edfe4a746
Joe Walker <joewalk102@gmail.com>
2022-06-03 10:12:56 -0700

    Expanded functionality of "print_grid" to automatically adjust to specified height & width.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import re
 
 
-def print_grid(words: list):
+def print_grid(words: list, width=5, height=8):
     cursor = 0
-    print("-" * 37 + "\n")
-    while cursor < len(words):
-        print("\t".join(words[cursor : cursor + 5]), end="\n\n")
-        cursor += 5
-    print("-" * 37)
+    border_len = min(((5 * width) + 8), (len(words) * 7) - 2)
+    print("-" * border_len + "\n")
+    rows = 1
+    while cursor < len(words) and rows < height:
+        print("  ".join(words[cursor : cursor + width]), end="\n\n")
+        cursor += width
+        rows += 1
+    print("-" * border_len)
 
 
 class Solver:
@@ -34,15 +37,14 @@ class Solver:
                 self.required_letters.add(letter)
 
     def add_to_negative(self, new_negatives):
-        new_negatives = {neg for neg in new_negatives if neg not in self.required_letters}
-        self.negative_letters = self.negative_letters | set(new_negatives)
+        new_negatives = {
+            neg for neg in new_negatives if neg not in self.required_letters
+        }
+        self.negative_letters = self.negative_letters | new_negatives
 
-    def find_words(self):
-        if not self.pattern:
-            return self.words.splitlines()
-        matches = re.findall(f"^{self.pattern}$", self.words, flags=re.M)
+    def _filter_for_required_and_negative(self, matches):
+        final_matches = list()
         if self.required_letters or self.negative_letters:
-            final_matches = list()
             for m in matches:
                 if self.required_letters and not all(
                     [letter in m for letter in self.required_letters]
@@ -53,8 +55,13 @@ class Solver:
                 ):
                     continue
                 final_matches.append(m)
-            return final_matches
-        return matches
+        return final_matches or matches
+
+    def find_words(self):
+        if not self.pattern:
+            return self.words.splitlines()
+        matches = re.findall(f"^{self.pattern}$", self.words, flags=re.M)
+        return self._filter_for_required_and_negative(matches)
 
 
 def main():
@@ -64,9 +71,14 @@ def main():
     solver = Solver(words=words)
     while go_again:
         raw_text = input("whatcha got? -> ")
+        # Expected pattern (separated by spaces):
+        # "?" for unknown letter (black)
+        # "?l" for unknown position (yellow)
+        # "l" for known letter and position (green)
         solver.parse_input(raw_text)
 
         print("Current omitted letters: " + " ".join(solver.negative_letters))
+        # Expected: All black letters with no spaces.
         solver.add_to_negative(input("what's not there? -> "))
 
         print_grid(solver.find_words()[:40])
